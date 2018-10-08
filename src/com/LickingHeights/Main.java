a57a6835e6ababe4307abd587d83b899766a7ca5
McKenna Cash <mckenna.cash.2020@student.lhschools.org>
2018-10-08 07:57:17 -0400

    uwuwuwuwuwuwwuwuwuwuwuuwuwuwuwwuwuuw/"

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,30 @@
+package com.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner keyboard;
+
+        keyboard = new Scanner(System.in);
+
+        System.out.println("Want to play rock paper scissors?");
+        keyboard.nextLine();
+
+
+ int pickedNumber;
+
+
+
+do {
+    pickedNumber = (int) (Math.random() * 100);
+    System.out.println(pickedNumber);
+}while (true);
+
+
+
+
+
+    }
+}
